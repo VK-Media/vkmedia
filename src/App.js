7359e808ed900e6f51b72a5ce9068ae7d086a915
Robin <rlvk94@gmail.com>
2018-12-29 20:09:35 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/logo_cobalt.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div id="temp-content">
+					<img id="logo" src={logo} alt="logo" />
+					<h1>Hjemmesider og webapps i alle størrelser</h1>
+					<p>Vi specialiserer os i at lave hjemmesider og webapps i alle størrelser til alle typer af kunder.</p>
+					<p>Er du interesseret i at høre nærmere, kan du kontakte os på nedenstående email</p>
+					<a className="color5" href="mailto:kontakt@vkmedia.dk">kontakt@vkmedia.dk</a>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
